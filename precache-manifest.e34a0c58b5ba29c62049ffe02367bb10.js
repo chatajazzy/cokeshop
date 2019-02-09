24cbdace2fc4bb895a130f1c1c3659c9ca83eb80
chatajazzy <chatajazzy@interia.pl>
2019-02-09 14:58:11 +0100

    Updates

---
+++ precache-manifest.e34a0c58b5ba29c62049ffe02367bb10.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5b877d5ca8c0f76766df",
+    "url": "/cokeshop/static/js/runtime~main.5b877d5c.js"
+  },
+  {
+    "revision": "0ea7533f8c38500713ea",
+    "url": "/cokeshop/static/js/main.0ea7533f.chunk.js"
+  },
+  {
+    "revision": "5cabbdc29baec113d4ea",
+    "url": "/cokeshop/static/js/1.5cabbdc2.chunk.js"
+  },
+  {
+    "revision": "0ea7533f8c38500713ea",
+    "url": "/cokeshop/static/css/main.de416cec.chunk.css"
+  },
+  {
+    "revision": "a04801961ac8c08335735d06720ea811",
+    "url": "/cokeshop/index.html"
+  }
+];
